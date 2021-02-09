3f8dc96f09378100cd70ab70d1bcc56d9a2dee79
Saloni83 <75675666+Saloni83@users.noreply.github.com>
2021-02-09 16:23:04 +0530

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,37 @@
+class Block{
+    constructor(x, y, width, height, angle) {
+        var options = {
+          restitution : 0.7,
+          friction : 0.6,
+          isStatic : false,
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+       
+        this.visibility=255;
+
+         World.add(world, this.body);
+      }
+      display(){
+        var pos = this.body.position;
+        var angle = this.angle;
+        push();
+        if (this.body.speed < 3) {
+            rectMode(CENTER);
+            translate(pos.x, pos.y);
+            rotate(angle);
+            fill(color);
+            stroke(border);
+            strokeWeight(4);
+            rect(0, 0, this.width, this.height);
+            pop();
+        } else {
+            push();
+            World.remove(world, this.body);
+            this.visibility = this.visibility - 5;
+            tint(255, this.visibility);
+            pop();
+        }
+      }
+}
